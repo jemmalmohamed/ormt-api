670ded8b186eab8e077185fc06554d8f67f2f464
jemmalmohamed <jemmalmohamed@gmail.com>
2024-06-20 17:20:54 +0100

    add vari

--- docker/services/sqlserver/env/init-db.sh
+++ docker/services/sqlserver/env/init-db.sh
@@ -2,11 +2,16 @@
 
 # Wait for SQL Server to start
 echo "Waiting for SQL Server to start..."
-until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SQLSERVER_API_PASSWORD -Q "SELECT 1" > /dev/null 2>&1; do
-  echo "Waiting for SQL Server to be available..."
-  sleep 5
-done
+sleep 30s
+
+
+echo "SQL Server is available. Running initialization commands."
 
 # Run SQL commands to create the database and enable spatial support
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SQLSERVER_API_PASSWORD -Q "CREATE DATABASE [$SQLSERVER_API_DB]"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SQLSERVER_API_PASSWORD -d $SQLSERVER_API_DB -Q "CREATE TABLE SpatialTable (ID INT PRIMARY KEY, Location GEOMETRY)"
+echo "Creating database: $SQLSERVER_API_DB"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SQLSERVER_API_PASSWORD" -Q "CREATE DATABASE [$SQLSERVER_API_DB]"
+
+echo "Creating table: SpatialTable"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SQLSERVER_API_PASSWORD" -d "$SQLSERVER_API_DB" -Q "CREATE TABLE SpatialTable (ID INT PRIMARY KEY, Location GEOMETRY)"
+
+echo "Initialization script completed."
