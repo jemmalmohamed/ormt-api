db96af279638a968f8c2df6e75bc5c7a403a0e7e
jemmalmohamed <jemmalmohamed@gmail.com>
2024-06-20 17:50:40 +0100

    add command

--- docker/services/sqlserver/env/init-db-stage.sh
+++ docker/services/sqlserver/env/init-db-stage.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Print environment variables
-echo "SQLSERVER_API_PASSWORD: ${SQLSERVER_API_PASSWORD}"
-echo "SQLSERVER_API_DB: ${SQLSERVER_API_DB}"
+FLAG_FILE="/var/opt/mssql/data/.init-db-completed"
+
+# Check if the initialization has already been completed
+if [ -f "${FLAG_FILE}" ]; then
+  echo "Initialization already completed. Exiting."
+  exit 0
+fi
+
+ 
 
 # Wait for SQL Server to start
 echo "Waiting for SQL Server to start..."
-timeout=10  # Timeout in seconds 
+timeout=10  # Timeout in seconds
 interval=3  # Sleep interval in seconds
 elapsed=0
 
@@ -22,9 +28,11 @@ done
 
 echo "SQL Server is available. Running initialization commands."
 
-# Run SQL commands to create the database and enable spatial support
+# Run SQL commands to create the database
 echo "Creating database: ${SQLSERVER_API_DB}"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SQLSERVER_API_PASSWORD}" -Q "CREATE DATABASE [${SQLSERVER_API_DB}]"
 
- 
+# Create a flag file to indicate that the initialization has been completed
+touch "${FLAG_FILE}"
+
 echo "Initialization script completed."
