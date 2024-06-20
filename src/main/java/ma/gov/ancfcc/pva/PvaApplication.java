8b9c6987067b4caefa0dd0757cb7f1bdae5141d1
jemmalmohamed <jemmalmohamed@gmail.com>
2024-06-20 14:58:13 +0100

    install java 22

---
+++ src/main/java/ma/gov/ancfcc/pva/PvaApplication.java
@@ -0,0 +1,13 @@
+package ma.gov.ancfcc.pva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/gov/ancfcc/pva/PvaApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.gov.ancfcc.pva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
