a9da33bbf7139d1f7cce3743b6e56e46a25f412b
jemmalmohamed <jemmalmohamed@gmail.com>
2024-06-20 16:30:28 +0100

    add init sh

---
+++ docker/services/sqlserver/env/init-db-stage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Wait for SQL Server to start
+sleep 30s
+
+# Run SQL commands to create the database and enable spatial support
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SQLSERVER_API_PASSWORD} -d master -Q "CREATE DATABASE ${SQLSERVER_API_DB}"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SQLSERVER_API_PASSWORD} -d ${SQLSERVER_API_DB} -Q "CREATE TABLE SpatialTable (ID INT PRIMARY KEY, Location GEOMETRY)"
