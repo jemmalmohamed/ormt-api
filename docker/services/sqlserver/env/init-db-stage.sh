96d39a6ffa35f825b8d5d54343301cc6b2a8f43c
jemmalmohamed <jemmalmohamed@gmail.com>
2024-06-21 21:27:55 +0100

    fix sqlserver

--- docker/services/sqlserver/env/init-db-stage.sh
+++ docker/services/sqlserver/env/init-db-stage.sh
@@ -2,12 +2,11 @@
 
 echo "SQL Server is available. Running initialization commands."
 
-# Run SQL commands to create the database
-echo "Creating database: ${SQLSERVER_API_DB}"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SQLSERVER_API_PASSWORD}" -Q "CREATE DATABASE [${SQLSERVER_API_DB}]"
+# Check if the database already exists
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SQLSERVER_API_PASSWORD}" -Q "IF DB_ID('${SQLSERVER_API_DB}') IS NULL CREATE DATABASE [${SQLSERVER_API_DB}]"
 
 if [ $? -eq 0 ]; then
-    echo "Database ${SQLSERVER_API_DB} created successfully."
+    echo "Database ${SQLSERVER_API_DB} created successfully or already exists."
 else
     echo "Error creating database ${SQLSERVER_API_DB}."
     exit 1
