42aa681716b8c4b0f8be364a0603660cf453d6e3
jemmalmohamed <jemmalmohamed@gmail.com>
2024-06-21 22:06:57 +0100

    test dockefile

--- docker/services/sqlserver/env/init-db-stage.sh
+++ docker/services/sqlserver/env/init-db-stage.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-echo "SQL Server is available. Running initialization commands."
+# Check if the init script has already run
+if [ -f /var/opt/mssql/data/.init_done ]; then
+  echo "Database already initialized"
+  exit 0
+fi
 
-# Check if the database already exists
+# Wait for SQL Server to start up
+sleep 30s
+
+# Run your initialization SQL scripts
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SQLSERVER_API_PASSWORD}" -Q "IF DB_ID('${SQLSERVER_API_DB}') IS NULL CREATE DATABASE [${SQLSERVER_API_DB}]"
 
-if [ $? -eq 0 ]; then
-    echo "Database ${SQLSERVER_API_DB} created successfully or already exists."
-else
-    echo "Error creating database ${SQLSERVER_API_DB}."
-    exit 1
-fi
+# Create a flag file to indicate that the initialization has been done
+touch /var/opt/mssql/data/.init_done
 
 echo "Initialization script completed."
