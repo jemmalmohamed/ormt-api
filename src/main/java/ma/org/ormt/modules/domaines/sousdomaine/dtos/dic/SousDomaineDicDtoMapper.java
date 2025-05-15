991b225676b088bf40b86bf829a923adc18178ef
jemmalmohamed <jemmalmohamed@gmail.com>
2025-05-15 17:54:51 +0100

    add data

--- src/main/java/ma/org/ormt/modules/domaines/sousdomaine/dtos/dic/SousDomaineDicDtoMapper.java
+++ src/main/java/ma/org/ormt/modules/domaines/sousdomaine/dtos/dic/SousDomaineDicDtoMapper.java
@@ -4,11 +4,7 @@ import org.mapstruct.Mapper;
 
 import ma.org.ormt.core.commun.base.mapper.BaseDtoMapper;
 import ma.org.ormt.modules.domaines.sousdomaine.models.SousDomaine;
-import ma.org.ormt.modules.indicateurs.indicateur.dtos.series.IndicateurChartDtoMapper;
 
-/**
- * Mapper interface for converting between SousDomaine and SousDomaineDicDto.
- */
 @Mapper()
 public interface SousDomaineDicDtoMapper extends BaseDtoMapper<SousDomaine, SousDomaineDicDto> {
 
