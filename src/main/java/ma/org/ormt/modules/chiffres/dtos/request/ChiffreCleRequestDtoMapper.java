ad8cef990a0211b01c1d5a6ab5e5e34bd23099ca
jemmalmed <jemmalmohamed@gmail.com>
2026-05-15 14:49:26 +0100

    Update dependencies in pom.xml and refactor application-dev.yml for development configuration

--- src/main/java/ma/org/ormt/modules/chiffres/dtos/request/ChiffreCleRequestDtoMapper.java
+++ src/main/java/ma/org/ormt/modules/chiffres/dtos/request/ChiffreCleRequestDtoMapper.java
@@ -1,5 +1,8 @@
 package ma.org.ormt.modules.chiffres.dtos.request;
 
+import java.util.List;
+
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 
 import ma.org.ormt.core.commun.base.mapper.BaseDtoMapper;
@@ -7,4 +10,15 @@ import ma.org.ormt.modules.chiffres.models.ChiffreCle;
 
 @Mapper
 public interface ChiffreCleRequestDtoMapper extends BaseDtoMapper<ChiffreCle, ChiffreCleRequestDto> {
+
+	@Override
+	default List<ChiffreCleRequestDto> mapToDto(List<ChiffreCle> list, @Context Object... services) {
+		if (list == null) {
+			return null;
+		}
+
+		return list.stream()
+				.map(entity -> mapToDto(entity, services))
+				.toList();
+	}
 }
